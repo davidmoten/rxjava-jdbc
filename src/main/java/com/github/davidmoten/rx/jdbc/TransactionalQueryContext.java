1e955fd07b5f8d14d7586a29a30d14b2da86fd1d
Dave Moten <davidmoten@gmail.com>
2014-02-11 22:28:39 +1100

    add javadoc

--- src/main/java/com/github/davidmoten/rx/jdbc/TransactionalQueryContext.java
+++ src/main/java/com/github/davidmoten/rx/jdbc/TransactionalQueryContext.java
@@ -1,12 +1,22 @@
 package com.github.davidmoten.rx.jdbc;
 
+import java.sql.Connection;
 import java.util.concurrent.Executors;
 
 import com.github.davidmoten.rx.jdbc.connection.ConnectionProvider;
 import com.github.davidmoten.rx.jdbc.connection.SingletonManualCommitConnectionProvider;
 
-public class TransactionalQueryContext extends QueryContext {
+/**
+ * {@link QueryContext} for a transaction. Returns an executor based on a single
+ * threaded pool and a cached non-auto commit {@link Connection}.
+ */
+class TransactionalQueryContext extends QueryContext {
 
+	/**
+	 * Constructor.
+	 * 
+	 * @param connectionProvider
+	 */
 	TransactionalQueryContext(ConnectionProvider connectionProvider) {
 		super(Executors.newSingleThreadExecutor(),
 				new SingletonManualCommitConnectionProvider(connectionProvider));
