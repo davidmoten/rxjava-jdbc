7bd34abf1c34d990a9a1a4ba46b4bb7243c2686b
Dave Moten <davidmoten@gmail.com>
2014-03-03 07:57:00 +1100

    add unit test for Conditions

--- src/test/java/com/github/davidmoten/rx/jdbc/ConditionsTest.java
+++ src/test/java/com/github/davidmoten/rx/jdbc/ConditionsTest.java
@@ -5,13 +5,33 @@ import org.junit.Test;
 public class ConditionsTest {
 
 	@Test
-	public void testTrueDoesNotThrowException() {
+	public void testCheckTrueDoesNotThrowException() {
 		Conditions.checkTrue(true);
 	}
 
 	@Test(expected = RuntimeException.class)
-	public void testFalseThrowsException() {
+	public void testCheckTrueThrowsException() {
 		Conditions.checkTrue(false);
+	}
+
+	@Test
+	public void testCheckFalseDoesNotThrowException() {
+		Conditions.checkFalse(false);
+	}
+
+	@Test(expected = RuntimeException.class)
+	public void testCheckFalseThrowsException() {
+		Conditions.checkFalse(true);
+	}
+
+	@Test
+	public void testCheckNotNullDoesNotThrowException() {
+		Conditions.checkNotNull(new Object());
+	}
+
+	@Test(expected = RuntimeException.class)
+	public void testCheckNotNullThrowsException() {
+		Conditions.checkNotNull(null);
 	}
 
 	@Test
