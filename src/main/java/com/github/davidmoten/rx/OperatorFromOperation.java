2e51daf55e89560e80572e55e136bc71f43d1ed3
Dave Moten <davidmoten@gmail.com>
2014-03-03 12:02:57 +1100

    add javadoc

--- src/main/java/com/github/davidmoten/rx/OperatorFromOperation.java
+++ src/main/java/com/github/davidmoten/rx/OperatorFromOperation.java
@@ -6,10 +6,28 @@ import rx.Subscriber;
 import rx.functions.Func1;
 import rx.subjects.PublishSubject;
 
+/**
+ * Converts an Operation (a function converting one Observable into another)
+ * into an {@link Operator}.
+ * 
+ * @param <R>
+ *            to type
+ * @param <T>
+ *            from type
+ */
 public class OperatorFromOperation<R, T> implements Operator<R, T> {
 
+	/**
+	 * The operation to convert.
+	 */
 	private final Func1<Observable<T>, Observable<R>> operation;
 
+	/**
+	 * Constructor.
+	 * 
+	 * @param operation
+	 *            to be converted into {@link Operator}
+	 */
 	public OperatorFromOperation(Func1<Observable<T>, Observable<R>> operation) {
 		this.operation = operation;
 	}
@@ -18,7 +36,21 @@ public class OperatorFromOperation<R, T> implements Operator<R, T> {
 	public Subscriber<? super T> call(Subscriber<? super R> subscriber) {
 		final PublishSubject<T> subject = PublishSubject.create();
 		operation.call(subject).subscribe(subscriber);
-		Subscriber<T> result = new Subscriber<T>() {
+		Subscriber<T> result = createSubscriber(subject);
+		subscriber.add(result);
+		return result;
+	}
+
+	/**
+	 * Creates a subscriber that passes all events on to the subject.
+	 * 
+	 * @param subject
+	 *            receives all events.
+	 * @return
+	 */
+	private static <T> Subscriber<T> createSubscriber(
+			final PublishSubject<T> subject) {
+		return new Subscriber<T>() {
 
 			@Override
 			public void onCompleted() {
@@ -35,7 +67,5 @@ public class OperatorFromOperation<R, T> implements Operator<R, T> {
 				subject.onNext(t);
 			}
 		};
-		subscriber.add(result);
-		return result;
 	}
 }
