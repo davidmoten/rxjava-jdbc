874dd9cf29ea065f7ebf453fd27ba89922f35737
Dave Moten <davidmoten@gmail.com>
2014-03-12 10:55:06 +1100

    don't need connection to begin transaction

--- src/main/java/com/github/davidmoten/rx/jdbc/QueryUpdateOperation.java
+++ src/main/java/com/github/davidmoten/rx/jdbc/QueryUpdateOperation.java
@@ -84,17 +84,18 @@ class QueryUpdateOperation {
         public void call(Subscriber<? super Integer> subscriber) {
             try {
 
-                getConnection();
                 if (isBeginTransaction())
                     performBeginTransaction(subscriber);
-                else if (isCommit())
-                    performCommit(subscriber);
-                else if (isRollback())
-                    performRollback(subscriber);
-                else
-                    performUpdate(subscriber);
-
-                close();
+                else {
+                	getConnection();
+                	if (isCommit())
+                        performCommit(subscriber);
+                    else if (isRollback())
+                        performRollback(subscriber);
+                    else
+                        performUpdate(subscriber);
+                	close();
+                }
 
                 complete(subscriber);
 
