1a1bc6409a14fc97f01cfeedc0b759184a9219d4
Dave Moten <davidmoten@gmail.com>
2014-03-18 11:06:27 +1100

    add unit tests

--- src/test/java/com/github/davidmoten/rx/OperationToOperatorTest.java
+++ src/test/java/com/github/davidmoten/rx/OperationToOperatorTest.java
@@ -3,6 +3,7 @@ package com.github.davidmoten.rx;
 import static com.github.davidmoten.rx.OperationToOperator.toOperator;
 import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
@@ -12,7 +13,7 @@ import java.util.concurrent.TimeUnit;
 import org.junit.Test;
 
 import rx.Observable;
-import rx.functions.Action1;
+import rx.Observer;
 import rx.functions.Functions;
 import rx.subjects.PublishSubject;
 
@@ -77,20 +78,69 @@ public class OperationToOperatorTest {
         subject.onNext(2);
         assertEquals(asList(1, 2), recorder1.list());
         assertEquals(asList(1, 2), recorder2.list());
+        assertFalse(recorder1.isCompleted());
+        assertFalse(recorder2.isCompleted());
         subject.onCompleted();
+        assertTrue(recorder1.isCompleted());
+        assertTrue(recorder2.isCompleted());
+    }
+    
+    @Test
+    public void testErrorsPassedThroughToOperator() {
+        PublishSubject<Integer> subject = PublishSubject.create();
+        Recorder recorder1 = new Recorder();
+        Recorder recorder2 = new Recorder();
+        subject.subscribe(recorder1);
+        subject.subscribe(recorder2);
+        subject.onNext(1);
+        assertEquals(asList(1), recorder1.list());
+        assertEquals(asList(1), recorder2.list());
+        subject.onNext(2);
+        assertEquals(asList(1, 2), recorder1.list());
+        assertEquals(asList(1, 2), recorder2.list());
+        Exception e = new Exception("boo");
+        assertTrue(recorder1.errors().isEmpty());
+        assertTrue(recorder2.errors().isEmpty());
+        subject.onError(e);
+        assertEquals(asList(e),recorder1.errors());
+        assertEquals(asList(e),recorder2.errors());
     }
 
-    private static class Recorder implements Action1<Integer> {
+    
+    private static class Recorder implements Observer<Integer> {
 
-        private final List<Integer> list = new ArrayList<Integer>();
-
-        @Override
-        public void call(Integer t) {
-            list.add(t);
+        private final List<Throwable> errors = new ArrayList<Throwable>();
+        private final List<Integer> list= new ArrayList<Integer>();
+        private boolean completed= false;
+        
+        boolean isCompleted() {
+            return completed;
         }
 
+        List<Throwable> errors() {
+            return errors;
+        }
+        
         List<Integer> list() {
             return list;
+        }
+
+
+        @Override
+        public void onCompleted() {
+            completed=true;
+        }
+
+
+        @Override
+        public void onError(Throwable e) {
+            errors.add(e);
+        }
+
+
+        @Override
+        public void onNext(Integer t) {
+            list.add(t);
         }
 
     }
