b32150aa66ec000385ae6f66bd75387834860225
Dave Moten <davidmoten@gmail.com>
2014-03-22 17:52:51 +1100

    add javadoc and use RxUtil static methods

--- src/main/java/com/github/davidmoten/rx/RxUtil.java
+++ src/main/java/com/github/davidmoten/rx/RxUtil.java
@@ -14,123 +14,138 @@ import rx.functions.Action1;
 import rx.functions.Func1;
 import rx.functions.Functions;
 
+/**
+ * Utility methods for RxJava.
+ */
 public class RxUtil {
 
-    /**
-     * slf4j logger.
-     */
-    private static final Logger log = LoggerFactory.getLogger(RxUtil.class);
+	/**
+	 * slf4j logger.
+	 */
+	private static final Logger log = LoggerFactory.getLogger(RxUtil.class);
 
-    /**
-     * Returns the concatenation of two {@link Observable}s but the first
-     * sequence will be emitted in its entirety and ignored before o2 starts
-     * emitting.
-     * 
-     * @param o1
-     *            the sequence to ignore
-     * @param o2
-     *            the sequence to emit after o1 ignored
-     * @return
-     */
-    @SuppressWarnings("unchecked")
-    public static <T> Observable<T> concatButIgnoreFirstSequence(Observable<?> o1, Observable<T> o2) {
-        return Observable.concat((Observable<T>) o1.ignoreElements(), o2);
-    }
+	/**
+	 * Returns the concatenation of two {@link Observable}s but the first
+	 * sequence will be emitted in its entirety and ignored before o2 starts
+	 * emitting.
+	 * 
+	 * @param o1
+	 *            the sequence to ignore
+	 * @param o2
+	 *            the sequence to emit after o1 ignored
+	 * @return
+	 */
+	@SuppressWarnings("unchecked")
+	public static <T> Observable<T> concatButIgnoreFirstSequence(
+			Observable<?> o1, Observable<T> o2) {
+		return Observable.concat((Observable<T>) o1.ignoreElements(), o2);
+	}
 
-    /**
-     * Logs errors and onNext at info level using slf4j {@link Logger}.
-     * 
-     * @return
-     */
-    public static <T> Observer<? super T> log() {
-        return new Observer<T>() {
+	/**
+	 * Logs errors and onNext at info level using slf4j {@link Logger}.
+	 * 
+	 * @return
+	 */
+	public static <T> Observer<? super T> log() {
+		return new Observer<T>() {
 
-            @Override
-            public void onCompleted() {
-                // do nothing
-            }
+			@Override
+			public void onCompleted() {
+				// do nothing
+			}
 
-            @Override
-            public void onError(Throwable e) {
-                log.error(e.getMessage(), e);
-            }
+			@Override
+			public void onError(Throwable e) {
+				log.error(e.getMessage(), e);
+			}
 
-            @Override
-            public void onNext(T t) {
-                log.info(t + "");
-            }
-        };
-    }
+			@Override
+			public void onNext(T t) {
+				log.info(t + "");
+			}
+		};
+	}
 
-    /**
-     * Returns a constant value.
-     * 
-     * @param s
-     * @return
-     */
-    public static <R, S> Func1<R, S> constant(final S s) {
-        return new Func1<R, S>() {
-            @Override
-            public S call(R t1) {
-                return s;
-            }
-        };
-    }
+	/**
+	 * Returns a constant value.
+	 * 
+	 * @param s
+	 * @return
+	 */
+	public static <R, S> Func1<R, S> constant(final S s) {
+		return new Func1<R, S>() {
+			@Override
+			public S call(R t1) {
+				return s;
+			}
+		};
+	}
 
-    public static <R, T> Operator<R, T> toOperator(Func1<Observable<T>, Observable<R>> operation) {
-        return OperationToOperator.toOperator(operation);
-    }
+	/**
+	 * Converts a transformation of an Observable into another Observable into
+	 * an {@link Operator} suitable for use with
+	 * {@link Observable#lift(Operator)} for instance.
+	 * 
+	 * @param operation
+	 * @return
+	 */
+	public static <R, T> Operator<R, T> toOperator(
+			Func1<Observable<T>, Observable<R>> operation) {
+		return OperationToOperator.toOperator(operation);
+	}
 
-    public static <T> UnsubscribeDetector<T> detectUnsubscribe() {
-        return UnsubscribeDetector.detect();
-    }
+	public static <T> UnsubscribeDetector<T> detectUnsubscribe() {
+		return UnsubscribeDetector.detect();
+	}
 
-    public static <T> CountingAction<T> counter() {
-        return new CountingAction<T>();
-    }
+	public static <T> CountingAction<T> counter() {
+		return new CountingAction<T>();
+	}
 
-    public static class CountingAction<T> implements Action1<T> {
-        private final AtomicLong count = new AtomicLong(0);
+	public static class CountingAction<T> implements Action1<T> {
+		private final AtomicLong count = new AtomicLong(0);
 
-        public Observable<Long> count() {
-            return Observable.create(new OnSubscribe<Long>() {
+		public Observable<Long> count() {
+			return Observable.create(new OnSubscribe<Long>() {
 
-                @Override
-                public void call(Subscriber<? super Long> subscriber) {
-                    subscriber.onNext(count.get());
-                    subscriber.onCompleted();
-                }
-            });
-        }
+				@Override
+				public void call(Subscriber<? super Long> subscriber) {
+					subscriber.onNext(count.get());
+					subscriber.onCompleted();
+				}
+			});
+		}
 
-        @Override
-        public void call(T t) {
-            count.incrementAndGet();
-        }
-    }
+		@Override
+		public void call(T t) {
+			count.incrementAndGet();
+		}
+	}
 
-    public static <T extends Number> Func1<T, Boolean> greaterThanZero() {
-        return new Func1<T, Boolean>() {
+	public static <T extends Number> Func1<T, Boolean> greaterThanZero() {
+		return new Func1<T, Boolean>() {
 
-            @Override
-            public Boolean call(T t) {
-                return t.doubleValue() > 0;
-            }
-        };
-    }
+			@Override
+			public Boolean call(T t) {
+				return t.doubleValue() > 0;
+			}
+		};
+	}
 
-    public static <T> Func1<T, Observable<Object>> toEmpty() {
-        return constant(Observable.<Object> empty());
-    }
+	public static <T> Func1<T, Observable<Object>> toEmpty() {
+		return constant(Observable.<Object> empty());
+	}
 
-    public static <T> Operator<T, Observable<T>> flatten() {
-        return RxUtil.toOperator(new Func1<Observable<Observable<T>>, Observable<T>>() {
+	public static <T> Operator<T, Observable<T>> flatten() {
+		return RxUtil
+				.toOperator(new Func1<Observable<Observable<T>>, Observable<T>>() {
 
-            @Override
-            public Observable<T> call(Observable<Observable<T>> source) {
-                return source.flatMap(Functions.<Observable<T>> identity());
-            }
-        });
-    }
+					@Override
+					public Observable<T> call(Observable<Observable<T>> source) {
+						return source.flatMap(Functions
+								.<Observable<T>> identity());
+					}
+				});
+	}
 
 }
--- src/test/java/com/github/davidmoten/rx/OperationToOperatorTest.java
+++ src/test/java/com/github/davidmoten/rx/OperationToOperatorTest.java
@@ -19,130 +19,131 @@ import rx.subjects.PublishSubject;
 
 public class OperationToOperatorTest {
 
-    private static final int AWAIT_SECONDS = 10;
+	private static final int AWAIT_SECONDS = 10;
 
-    @Test
-    public void testUnsubscribeFromAsynchronousSource() throws InterruptedException {
+	@Test
+	public void testUnsubscribeFromAsynchronousSource()
+			throws InterruptedException {
 
-        UnsubscribeDetector<Long> detector = UnsubscribeDetector.detect();
-        Observable
-        // every 100ms
-                .interval(100, TimeUnit.MILLISECONDS)
-                // detect unsubscribe
-                .lift(detector)
-                // use toOperator
-                .lift(toOperator(Functions.<Observable<Long>> identity())).take(1).first()
-                // block and get result
-                .toBlockingObservable().single();
-        // wait for expected unsubscription
-        assertTrue(detector.latch().await(AWAIT_SECONDS, TimeUnit.SECONDS));
+		UnsubscribeDetector<Long> detector = RxUtil.detectUnsubscribe();
+		Observable
+				// every 100ms
+				.interval(100, TimeUnit.MILLISECONDS)
+				// detect unsubscribe
+				.lift(detector)
+				// use toOperator
+				.lift(toOperator(Functions.<Observable<Long>> identity()))
+				.take(1).first()
+				// block and get result
+				.toBlockingObservable().single();
+		// wait for expected unsubscription
+		assertTrue(detector.latch().await(AWAIT_SECONDS, TimeUnit.SECONDS));
 
-    }
+	}
 
-    @Test
-    public void testUnsubscribeFromSynchronousSource() throws InterruptedException {
-        UnsubscribeDetector<Integer> detector = UnsubscribeDetector.detect();
-        PublishSubject<Integer> subject = PublishSubject.create();
-        subject
-        // detect unsubscribe
-        .lift(detector)
-        // use toOperator
-                .lift(toOperator(Functions.<Observable<Integer>> identity()))
-                // get first only
-                .take(1)
-                // subscribe and ignore events
-                .subscribe();
-        subject.onNext(1);
-        //should have unsubscribed because of take(1)
-        assertTrue(detector.latch().await(AWAIT_SECONDS, TimeUnit.SECONDS));
-    }
+	@Test
+	public void testUnsubscribeFromSynchronousSource()
+			throws InterruptedException {
+		UnsubscribeDetector<Integer> detector = RxUtil.detectUnsubscribe();
+		PublishSubject<Integer> subject = PublishSubject.create();
+		subject
+		// detect unsubscribe
+		.lift(detector)
+		// use toOperator
+				.lift(toOperator(Functions.<Observable<Integer>> identity()))
+				// get first only
+				.take(1)
+				// subscribe and ignore events
+				.subscribe();
+		subject.onNext(1);
+		// should have unsubscribed because of take(1)
+		assertTrue(detector.latch().await(AWAIT_SECONDS, TimeUnit.SECONDS));
+	}
 
-    @Test
-    public void testMultipleNonSimultaeousSubscriptions() {
-        Observable<Integer> sequence = Observable.range(1, 3).lift(
-                toOperator(Functions.<Observable<Integer>> identity()));
-        assertEquals(asList(1, 2, 3), sequence.toList().toBlockingObservable().single());
-        assertEquals(asList(1, 2, 3), sequence.toList().toBlockingObservable().single());
-    }
+	@Test
+	public void testMultipleNonSimultaeousSubscriptions() {
+		Observable<Integer> sequence = Observable.range(1, 3).lift(
+				toOperator(Functions.<Observable<Integer>> identity()));
+		assertEquals(asList(1, 2, 3), sequence.toList().toBlockingObservable()
+				.single());
+		assertEquals(asList(1, 2, 3), sequence.toList().toBlockingObservable()
+				.single());
+	}
 
-    @Test
-    public void testMultipleSimultaneousSubscriptions() {
-        PublishSubject<Integer> subject = PublishSubject.create();
-        Recorder recorder1 = new Recorder();
-        Recorder recorder2 = new Recorder();
-        subject.subscribe(recorder1);
-        subject.subscribe(recorder2);
-        subject.onNext(1);
-        assertEquals(asList(1), recorder1.list());
-        assertEquals(asList(1), recorder2.list());
-        subject.onNext(2);
-        assertEquals(asList(1, 2), recorder1.list());
-        assertEquals(asList(1, 2), recorder2.list());
-        assertFalse(recorder1.isCompleted());
-        assertFalse(recorder2.isCompleted());
-        subject.onCompleted();
-        assertTrue(recorder1.isCompleted());
-        assertTrue(recorder2.isCompleted());
-    }
-    
-    @Test
-    public void testErrorsPassedThroughToOperator() {
-        PublishSubject<Integer> subject = PublishSubject.create();
-        Recorder recorder1 = new Recorder();
-        Recorder recorder2 = new Recorder();
-        subject.subscribe(recorder1);
-        subject.subscribe(recorder2);
-        subject.onNext(1);
-        assertEquals(asList(1), recorder1.list());
-        assertEquals(asList(1), recorder2.list());
-        subject.onNext(2);
-        assertEquals(asList(1, 2), recorder1.list());
-        assertEquals(asList(1, 2), recorder2.list());
-        Exception e = new Exception("boo");
-        assertTrue(recorder1.errors().isEmpty());
-        assertTrue(recorder2.errors().isEmpty());
-        subject.onError(e);
-        assertEquals(asList(e),recorder1.errors());
-        assertEquals(asList(e),recorder2.errors());
-    }
+	@Test
+	public void testMultipleSimultaneousSubscriptions() {
+		PublishSubject<Integer> subject = PublishSubject.create();
+		Recorder recorder1 = new Recorder();
+		Recorder recorder2 = new Recorder();
+		subject.subscribe(recorder1);
+		subject.subscribe(recorder2);
+		subject.onNext(1);
+		assertEquals(asList(1), recorder1.list());
+		assertEquals(asList(1), recorder2.list());
+		subject.onNext(2);
+		assertEquals(asList(1, 2), recorder1.list());
+		assertEquals(asList(1, 2), recorder2.list());
+		assertFalse(recorder1.isCompleted());
+		assertFalse(recorder2.isCompleted());
+		subject.onCompleted();
+		assertTrue(recorder1.isCompleted());
+		assertTrue(recorder2.isCompleted());
+	}
 
-    
-    private static class Recorder implements Observer<Integer> {
+	@Test
+	public void testErrorsPassedThroughToOperator() {
+		PublishSubject<Integer> subject = PublishSubject.create();
+		Recorder recorder1 = new Recorder();
+		Recorder recorder2 = new Recorder();
+		subject.subscribe(recorder1);
+		subject.subscribe(recorder2);
+		subject.onNext(1);
+		assertEquals(asList(1), recorder1.list());
+		assertEquals(asList(1), recorder2.list());
+		subject.onNext(2);
+		assertEquals(asList(1, 2), recorder1.list());
+		assertEquals(asList(1, 2), recorder2.list());
+		Exception e = new Exception("boo");
+		assertTrue(recorder1.errors().isEmpty());
+		assertTrue(recorder2.errors().isEmpty());
+		subject.onError(e);
+		assertEquals(asList(e), recorder1.errors());
+		assertEquals(asList(e), recorder2.errors());
+	}
 
-        private final List<Throwable> errors = new ArrayList<Throwable>();
-        private final List<Integer> list= new ArrayList<Integer>();
-        private boolean completed= false;
-        
-        boolean isCompleted() {
-            return completed;
-        }
+	private static class Recorder implements Observer<Integer> {
 
-        List<Throwable> errors() {
-            return errors;
-        }
-        
-        List<Integer> list() {
-            return list;
-        }
+		private final List<Throwable> errors = new ArrayList<Throwable>();
+		private final List<Integer> list = new ArrayList<Integer>();
+		private boolean completed = false;
 
+		boolean isCompleted() {
+			return completed;
+		}
 
-        @Override
-        public void onCompleted() {
-            completed=true;
-        }
+		List<Throwable> errors() {
+			return errors;
+		}
 
+		List<Integer> list() {
+			return list;
+		}
 
-        @Override
-        public void onError(Throwable e) {
-            errors.add(e);
-        }
+		@Override
+		public void onCompleted() {
+			completed = true;
+		}
 
+		@Override
+		public void onError(Throwable e) {
+			errors.add(e);
+		}
 
-        @Override
-        public void onNext(Integer t) {
-            list.add(t);
-        }
+		@Override
+		public void onNext(Integer t) {
+			list.add(t);
+		}
 
-    }
+	}
 
 }
