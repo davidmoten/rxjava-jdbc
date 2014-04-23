f7f061a4661bf17c5ccdf0ebd3c7e781e01a959b
Dave Moten <davidmoten@gmail.com>
2014-04-23 12:23:52 +1000

    update retry test to use only mocks

--- src/test/java/com/github/davidmoten/rx/RetryTest.java
+++ src/test/java/com/github/davidmoten/rx/RetryTest.java
@@ -1,5 +1,9 @@
 package com.github.davidmoten.rx;
 
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.mock;
+
 import org.junit.Test;
 import org.mockito.InOrder;
 import org.mockito.Mockito;
@@ -15,41 +19,46 @@ public class RetryTest {
 
     // TODO move this test as pull request to rxjava-core.
     /**
-     * Overlaps somewhat with testSourceObservableCallsUnsubscribe() but is
-     * simpler and synchronous. This test fails against 0.16.1-0.17.4, hangs on
-     * 0.17.5 and passes in 0.17.6 thanks to fix for issue #1027.
+     * This test checks in a simple and synchronous way that retry resubscribes
+     * after error. This test fails against 0.16.1-0.17.4, hangs on 0.17.5 and
+     * passes in 0.17.6 thanks to fix for issue #1027.
      */
+    @SuppressWarnings("unchecked")
     @Test
     public void testRetrySubscribesAgainAfterError() {
-        Action1<Integer> record = Mockito.mock(Action1.class);
-        InOrder inOrder = Mockito.inOrder(record);
+
+        // record emitted values with this action
+        Action1<Integer> record = mock(Action1.class);
+        InOrder inOrder = inOrder(record);
+
+        // always throw and exception with this action
+        Action1<Integer> throwException = mock(Action1.class);
+        doThrow(new RuntimeException()).when(throwException).call(Mockito.anyInt());
+
+        // create a retrying observable based on a PublishSubject
         PublishSubject<Integer> subject = PublishSubject.create();
         subject
         // record item
         .doOnNext(record)
         // throw a RuntimeException
-                .doOnNext(throwException())
+                .doOnNext(throwException)
                 // retry on error
                 .retry()
                 // subscribe and ignore
                 .subscribe();
+
         inOrder.verifyNoMoreInteractions();
+
         subject.onNext(1);
         inOrder.verify(record).call(1);
+
         subject.onNext(2);
         inOrder.verify(record).call(2);
+
         subject.onNext(3);
         inOrder.verify(record).call(3);
-        inOrder.verifyNoMoreInteractions();
-    }
 
-    private Action1<Integer> throwException() {
-        return new Action1<Integer>() {
-            @Override
-            public void call(Integer t1) {
-                throw new RuntimeException("boo");
-            }
-        };
+        inOrder.verifyNoMoreInteractions();
     }
 
 }
