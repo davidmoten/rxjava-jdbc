8dcc373f649f262bd668eb06f5f7a19620eeca3f
Dave Moten <davidmoten@gmail.com>
2014-08-20 21:44:24 +1000

    cleanup use of anonymous function

--- src/main/java/com/github/davidmoten/rx/jdbc/QuerySelectOperator.java
+++ src/main/java/com/github/davidmoten/rx/jdbc/QuerySelectOperator.java
@@ -4,6 +4,8 @@ import static com.github.davidmoten.rx.OperationToOperator.toOperator;
 
 import java.sql.ResultSet;
 
+import com.github.davidmoten.rx.jdbc.QuerySelect.Builder;
+
 import rx.Observable;
 import rx.Observable.Operator;
 import rx.Subscriber;
@@ -27,32 +29,46 @@ final class QuerySelectOperator<T, R> implements Operator<T, R> {
      */
     QuerySelectOperator(final QuerySelect.Builder builder, final Func1<ResultSet, T> function,
             final OperatorType operatorType) {
-        operator = toOperator(new Func1<Observable<R>, Observable<T>>() {
-
-            @Override
-            public Observable<T> call(Observable<R> observable) {
-                if (operatorType == OperatorType.PARAMETER)
-                    return builder.parameters(observable).get(function);
-                else if (operatorType == OperatorType.DEPENDENCY)
-                    // dependency
-                    return builder.dependsOn(observable).get(function);
-                else // PARAMETER_LIST
-                {
-                    @SuppressWarnings("unchecked")
-                    Observable<Observable<Object>> obs = (Observable<Observable<Object>>) observable;
-                    return obs.concatMap(new Func1<Observable<Object>, Observable<T>>() {
-                        @Override
-                        public Observable<T> call(Observable<Object> parameters) {
-                            return builder.parameters(parameters).get(function);
-                        }
-                    });
-                }
-            }
-        });
+        operator = toOperator(new ApplyQuerySelect<R,T>(builder, function, operatorType));
     }
 
     @Override
     public Subscriber<? super R> call(Subscriber<? super T> subscriber) {
         return operator.call(subscriber);
+    }
+
+    private static class ApplyQuerySelect<R, T> implements Func1<Observable<R>, Observable<T>> {
+
+        private Builder builder;
+        private Func1<ResultSet, T> function;
+        private OperatorType operatorType;
+
+        private ApplyQuerySelect(QuerySelect.Builder builder, Func1<ResultSet, T> function,
+                OperatorType operatorType) {
+            this.builder = builder;
+            this.function = function;
+            this.operatorType = operatorType;
+        }
+
+        @Override
+        public Observable<T> call(Observable<R> observable) {
+            if (operatorType == OperatorType.PARAMETER)
+                return builder.parameters(observable).get(function);
+            else if (operatorType == OperatorType.DEPENDENCY)
+                // dependency
+                return builder.dependsOn(observable).get(function);
+            else // PARAMETER_LIST
+            {
+                @SuppressWarnings("unchecked")
+                Observable<Observable<Object>> obs = (Observable<Observable<Object>>) observable;
+                return obs.concatMap(new Func1<Observable<Object>, Observable<T>>() {
+                    @Override
+                    public Observable<T> call(Observable<Object> parameters) {
+                        return builder.parameters(parameters).get(function);
+                    }
+                });
+            }
+        }
+
     }
 }
