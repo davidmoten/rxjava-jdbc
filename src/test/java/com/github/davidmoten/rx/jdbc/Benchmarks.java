52c33bff1674c24d334cec4e907961b96c177fcd
Dave Moten <davidmoten@gmail.com>
2015-06-11 13:19:47 +1000

    benchmarks dev

--- src/test/java/com/github/davidmoten/rx/jdbc/Benchmarks.java
+++ src/test/java/com/github/davidmoten/rx/jdbc/Benchmarks.java
@@ -11,10 +11,13 @@ import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
 
+import rx.Observable;
+import rx.functions.Func1;
+
 @State(Scope.Benchmark)
 public class Benchmarks {
 
-    public final Connection con = new ConnectionNonClosing(DatabaseCreator.nextConnection());
+    public Connection con = createConnection();
     public final Database db = Database.from(con);
 
     @Benchmark
@@ -31,6 +34,18 @@ public class Benchmarks {
                 .toList()
                 // go
                 .toBlocking().single();
+    }
+
+    private static Connection createConnection() {
+        Connection con = new ConnectionNonClosing(DatabaseCreator.nextConnection());
+        Database db = Database.from(con);
+        //insert another 1000 people
+        db.update("insert into person(name, score) values(?,?)").parameters(Observable.range(1,1000).concatMap(new Func1<Integer, Observable<Object>>() {
+            @Override
+            public Observable<Object> call(Integer n) {
+                return Observable.<Object>just("person" + n , n);
+            }}));
+        return con;
     }
 
     @Benchmark
