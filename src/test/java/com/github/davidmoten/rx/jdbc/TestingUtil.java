302882753f1b85383cdf527ff75ace4b85e73d2a
Dave Moten <davidmoten@gmail.com>
2015-09-07 11:52:25 +1000

    use junit-extras

---
+++ src/test/java/com/github/davidmoten/rx/jdbc/TestingUtil.java
@@ -0,0 +1,18 @@
+package com.github.davidmoten.rx.jdbc;
+
+import java.util.concurrent.CountDownLatch;
+
+import rx.functions.Action0;
+
+public final class  TestingUtil {
+    
+    public static Action0 countDown(final CountDownLatch latch) {
+        return new Action0() {
+
+            @Override
+            public void call() {
+                latch.countDown();
+            }
+        };
+    }
+}
