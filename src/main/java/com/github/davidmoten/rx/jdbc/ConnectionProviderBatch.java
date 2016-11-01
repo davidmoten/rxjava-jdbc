29a16724aa9199be1ce3a09b6adcc9f787002c19
Dave Moten <davidmoten@gmail.com>
2016-11-01 16:13:47 +1100

    add Preconditions check to ConnectionProviderBatch constructor

--- src/main/java/com/github/davidmoten/rx/jdbc/ConnectionProviderBatch.java
+++ src/main/java/com/github/davidmoten/rx/jdbc/ConnectionProviderBatch.java
@@ -1,27 +1,30 @@
 package com.github.davidmoten.rx.jdbc;
 
+import com.github.davidmoten.guavamini.Preconditions;
+
 class ConnectionProviderBatch implements ConnectionProvider {
 
-	private final int batchSize;
-	private final ConnectionProvider cp;
-	private ConnectionBatch con;
+    private final int batchSize;
+    private final ConnectionProvider cp;
+    private ConnectionBatch con;
 
-	ConnectionProviderBatch(ConnectionProvider cp, int batchSize) {
-		this.cp = cp;
-		this.batchSize = batchSize;
-	}
+    ConnectionProviderBatch(ConnectionProvider cp, int batchSize) {
+        Preconditions.checkNotNull(cp, "cp should not be null");
+        this.cp = cp;
+        this.batchSize = batchSize;
+    }
 
-	@Override
-	public ConnectionBatch get() {
-		if (con == null) {
-			con = new ConnectionBatch(cp.get(), batchSize);
-		}
-		return con;
-	}
+    @Override
+    public ConnectionBatch get() {
+        if (con == null) {
+            con = new ConnectionBatch(cp.get(), batchSize);
+        }
+        return con;
+    }
 
-	@Override
-	public void close() {
-		cp.close();
-	}
+    @Override
+    public void close() {
+        cp.close();
+    }
 
 }
